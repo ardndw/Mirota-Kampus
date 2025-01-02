d30d871e9fb6f59fc68804e5b3865d2e92b88ad9
ardndw <ardiandwicahya0@gmail.com>
2025-01-02 23:15:19 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+// script.js
+const countdownDate = new Date("Dec 31, 2023 23:59:59").getTime();
+
+const timer = setInterval(function() {
+    const now = new Date().getTime();
+    const distance = countdownDate - now;
+
+    const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 
